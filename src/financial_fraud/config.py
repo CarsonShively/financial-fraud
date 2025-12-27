8ad10541798d4db839e6bf3eebb2bf2db7548c78
Carson Shively <carsonshively24@gmail.com>
2025-12-26 21:46:38 -0600

    data job config section

---
+++ src/financial_fraud/config.py
@@ -0,0 +1,9 @@
+REPO_ID = "carson-shively/financial-fraud"
+REVISION = "main"
+
+OFFLINE_DATA = "data/bronze/offline.parquet"
+ONLINE_DATA = "data/bronze/online.parquet"
+
+TRAIN_DATA = "data/gold/train.parquet"
+
+DUCKDB_PATH = "data/db/fraud.duckdb"
