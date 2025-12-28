ccc2cefda7b773f4c956f5158a157dabcbd85d6c
Carson Shively <carsonshively24@gmail.com>
2025-12-28 02:15:17 -0600

    add redis configs add run id util

--- src/financial_fraud/config.py
+++ src/financial_fraud/config.py
@@ -1,9 +1,26 @@
 REPO_ID = "carson-shively/financial-fraud"
 REVISION = "main"
 
-OFFLINE_DATA = "data/bronze/offline.parquet"
+TRANSACTION_LOG = "data/bronze/offline.parquet"
 ONLINE_DATA = "data/bronze/online.parquet"
 
 TRAIN_DATA = "data/gold/train.parquet"
 
 DUCKDB_PATH = "data/db/fraud.duckdb"
+
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
+REDIS_DB = 1
+
+REDIS_BASE_PREFIX = "fraud:features:"
+REDIS_CURRENT_POINTER_KEY = "fraud:features:CURRENT"
+REDIS_RUN_META_PREFIX = "fraud:features:RUN_META:"
+
+REDIS_TTL_SECONDS = 0
+
+ENTITY_ORIG_COL = "name_orig"
+ENTITY_DEST_COL = "name_dest"
+
+LABEL_COL    = "is_fraud"
+
+FS_ENTITY_KEYS = [ENTITY_ORIG_COL, ENTITY_DEST_COL]
