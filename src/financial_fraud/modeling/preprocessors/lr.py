357a3fa1408aa58af825a9bce952a2cf510fad9d
Carson Shively <carsonshively24@gmail.com>
2026-01-06 07:49:18 -0600

    update lr preprocessor for v3 spec

--- src/financial_fraud/modeling/preprocessors/lr.py
+++ src/financial_fraud/modeling/preprocessors/lr.py
@@ -9,16 +9,23 @@ def preprocessor() -> ColumnTransformer:
     cat_ohe = ["type"]
     type_categories = [["payment", "transfer", "cash_out", "debit", "cash_in", "unknown"]]
 
+
     num_log1p_scale = [
         "amount",
-        "orig_txn_count", "orig_amount_sum",
-        "dest_txn_count", "dest_amount_sum",
-        "orig_amount_mean", "dest_amount_mean",
+        "dest_amount_sum_1h",
+        "dest_amount_sum_24h",
+        "dest_amount_mean_24h",
     ]
 
     num_scale_only = [
-        "step", "orig_last_step", "dest_last_step",
-        "orig_balance_delta_sum", "dest_balance_delta_sum",
+        "step",
+        "orig_balance_delta",
+        "orig_delta_minus_amount",
+        "dest_balance_delta",
+        "dest_delta_minus_amount",
+        "dest_txn_count_1h",
+        "dest_txn_count_24h",
+        "dest_last_gap_hours",
     ]
 
     ohe_pipeline = Pipeline(
