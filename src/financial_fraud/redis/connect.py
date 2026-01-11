c1d18938b6e85fcdd7c54ee4b42676a23bd6ede2
Carson Shively <carsonshively24@gmail.com>
2026-01-10 20:44:39 -0700

    improve parity

--- src/financial_fraud/redis/connect.py
+++ src/financial_fraud/redis/connect.py
@@ -5,7 +5,7 @@ import redis
 from financial_fraud.redis.infra import RedisConfig
 from financial_fraud.config import (
     REDIS_HOST, REDIS_PORT, REDIS_DB,
-    REDIS_LIVE_PREFIX, REDIS_RUN_META_PREFIX, DEST_BUCKET_N
+    REDIS_LIVE_PREFIX, REDIS_RUN_META_PREFIX, DEST_BUCKET_N, PARITY_DB
 )
 
 def redis_config() -> RedisConfig:
@@ -13,6 +13,16 @@ def redis_config() -> RedisConfig:
         host=REDIS_HOST,
         port=REDIS_PORT,
         db=REDIS_DB,
+        live_prefix=REDIS_LIVE_PREFIX,
+        run_meta_prefix=REDIS_RUN_META_PREFIX,
+        dest_bucket_N=DEST_BUCKET_N,
+    )
+    
+def parity_redis_config() -> RedisConfig:
+    return RedisConfig(
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        db=PARITY_DB,
         live_prefix=REDIS_LIVE_PREFIX,
         run_meta_prefix=REDIS_RUN_META_PREFIX,
         dest_bucket_N=DEST_BUCKET_N,
